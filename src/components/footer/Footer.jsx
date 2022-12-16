9f965b5132a8d7539c2d5925f3ac00f2089df819
Junior <juusoares99@gmail.com>
2022-12-16 15:51:24 -0300

    .

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './footer.css'
+import {FiInstagram, FiLinkedin, FiGithub} from 'react-icons/fi'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <a href="#header" className='footer__logo'>Eli Junior</a>
+
+      <ul className="pemarlinks">
+        <li><a href="#header">Home</a></li>
+        <li><a href="#about">Sobre mim</a></li>
+        <li><a href="#experience">Experiência</a></li>
+        <li><a href="#services">Serviços</a></li>
+        <li><a href="#portfolio">Portfolio</a></li>
+        <li><a href="#testimonials">Depoimentos</a></li>
+        <li><a href="#contact">Contato</a></li>
+      </ul>
+
+    <div className="footer__socials">
+      <a href="https://instagram.com"><FiInstagram/></a>
+      <a href="https://linkedin.com"><FiLinkedin/></a>
+      <a href="https://github.com"><FiGithub/></a>
+    </div>
+    </footer>
   )
 }
 
